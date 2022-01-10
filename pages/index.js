ef4ed7415cf7ae9d4f93aeb6a971b956836fa7ba
wakishi <hun73r@icloud.com>
2022-01-10 21:32:01 +0100

    Change head icon

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     >
       <Head>
         <title>Outagram</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://i.ibb.co/tDfqgzC/logo-square.png" />
       </Head>
       <Header />
       <Feed />
